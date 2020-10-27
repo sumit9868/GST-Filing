9ab05e93cb3391e4ba822da26d651bff56203c84
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-10-27 16:21:21 +0530

    Structure

---
+++ src/JS/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../CSS/App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from './Signup';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+
+          <Route path="/signup">
+            <Header />
+            <Signup />
+            <Footer/>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+            <Footer/>
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
