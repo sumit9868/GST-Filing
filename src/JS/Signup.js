681f422239f082e6cdbdf66c332f7d2eeb1c319f
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-10-29 01:31:16 +0530

    Styling of signup and home

--- src/JS/Signup.js
+++ src/JS/Signup.js
@@ -3,6 +3,8 @@ import firebase from "./firebase";
 import StyleFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { useStateValue } from "./StateProvider";
 import PersonalHome from "./PersonalHome";
+import logo from "../Media/logo.png";
+import '../CSS/Signup.css'
 
 
 var uiConfig = {
@@ -39,9 +41,20 @@ function Signup() {
     );
   } else {
     return (
-      <div>
-        <span>Sign up page</span>
+      <div className="signup">
+        <div className="signupText">
+          <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div className="signupBox">
+        <img
+            src={logo}
+            className="signupLogo"
+            alt="logo"
+          />
+          <h3>Some random Text1</h3>
+          <h3>Some random Text2</h3>
         <StyleFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
       </div>
     );
   }
