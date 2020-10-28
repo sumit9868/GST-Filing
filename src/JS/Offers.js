e083648069120f24df420f166c4f720fd97da8f1
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-10-28 19:50:42 +0530

    Styling

--- src/JS/Offers.js
+++ src/JS/Offers.js
@@ -3,18 +3,18 @@ import '../CSS/Offers.css'
 function Offers() {
     return (
         <div className="offers">
-            <div>
+            <div className="offers__tagline">
                <span> Todays Offers </span>
             </div>
 
             <div>
                 <div>
                     Online Gst Filing
-                    <div>
-<div>Get Filing Packedge</div>
-    <div>Recognised by Startup India Registration number:1234UCO3343</div>
-<div>Free Accounting & Banking App</div>
-                    </div>
+                <div>
+                    <div>Get Filing Packedge</div>
+                    <div>Recognised by Startup India Registration number:1234UCO3343</div>
+                    <div>Free Accounting & Banking App</div>
+                </div>
                 </div>
             </div>
         </div>
