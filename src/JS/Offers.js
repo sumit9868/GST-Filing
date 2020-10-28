aa8c7239b62a0d7fc68fab9b9dd35b4345b81133
Sumit <sumitsaurabh9868@gmail.com>
2020-10-28 20:30:04 +0530

    worked on offers.css

--- src/JS/Offers.js
+++ src/JS/Offers.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import '../CSS/Offers.css'
 function Offers() {
-    return (
+    return (<div className="offers__box">
         <div className="offers">
             <div className="offers__tagline">
-               <span> Todays Offers </span>
+                <span> Todays Offers </span>
             </div>
 
-            <div>
-                <div>
+            <div className="offerbox2">
+                <div className="tagline2">
                     Online Gst Filing
-                <div>
-                    <div>Get Filing Packedge</div>
-                    <div>Recognised by Startup India Registration number:1234UCO3343</div>
-                    <div>Free Accounting & Banking App</div>
                 </div>
+                <div>
+                    <div className="tagline3">Get Filing Packedge</div>
+                    <div className="tagline4">Recognised by Startup India Registration number:1234UCO3343</div>
+                    <div>Free Accounting & Banking App</div>
                 </div>
             </div>
         </div>
+    </div>
     )
 }
 
