21c0878662afa852c4deb00c2839928f3988caf6
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-10-28 09:52:17 +0530

    Making Header Component

--- src/JS/Header.js
+++ src/JS/Header.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../Media/logo.png'
+import Navbar from 'react-bootstrap/Navbar'
 
+import '../CSS/Header.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Header() {
     return (
-        <div className="header">
-            <p>header</p>
-        </div>
+        <>
+            <Navbar className="navbar" bg="dark">
+                <Navbar.Brand className="leftCantainor" href="/">
+                    <img
+                        src={logo}
+                        // width="100"
+                        // height="100"
+                        // className="d-inline-block align-top"
+                        className="companyLogo"
+                        alt="logo"
+                    />{' '}
+                    <span className="companyName" > Easy GST </span>
+                </Navbar.Brand>
+
+                <div className="rightCantainor">
+                    <Link to ="/signup" className="accountLink">
+                        <span className="account">Account</span>
+                    </Link>
+                </div>
+                    
+            </Navbar>
+        </>
+
     )
 }
 
