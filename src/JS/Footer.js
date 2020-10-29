58ac75cfb11336b9e770857e85074a92cfa6c8b3
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-10-30 03:22:16 +0530

    all component styling

--- src/JS/Footer.js
+++ src/JS/Footer.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import logo from "../Media/logo.png";
+import "../CSS/Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <p>footer</p>
+
+  return (
+    <Navbar className="footerNav" bg="dark">
+
+      <Navbar.Brand className="footerLeftContainer">
+        <Link to="/" className="footerLeftContainer__link"  >
+          <img src={logo} className="companyLogo" alt="logo" />{" "}
+          <span className="companyName"> Easy GST </span>
+        </Link>
+      </Navbar.Brand>
+
+
+      <div className="footerRight">
+        <span className="developersTitle">Developed by</span>
+        <div className="developers">
+
+          <a
+            className={"footer__link"}
+            href={`mailto: sumitsaurabh9868@gmail.com`}
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            Sumit Saurabh
+          </a>
+
+          <a
+            className={"footer__link"}
+            href={`mailto: prabhjotlamba2001@gmail.com `}
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            Prabhjot Singh
+            </a>
+
+          <a
+            className={"footer__link"}
+            href={`mailto: abhijeetvarun@gmail.com`}
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            Abhijeet Varun
+            </a>
+
         </div>
-    )
+      </div>
+    </Navbar>
+
+  )
 }
 
 export default Footer
--- src/JS/PersonalHome.js
+++ src/JS/PersonalHome.js
@@ -2,14 +2,37 @@ import React, { useState } from "react";
 import { auth, db } from "./firebase";
 import { firebaseApp } from "./firebase";
 import { useHistory } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import "../CSS/PersonalHome.css";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 function PersonalHome() {
+
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+
   const [fileUrl1, setFileUrl1] = useState("");
   const [fileUrl2, setFileUrl2] = useState("");
+  const [fileUrl3, setFileUrl3] = useState("");
+  const [fileUrl4, setFileUrl4] = useState("");
+  const [fileUrl5, setFileUrl5] = useState("");
+  const [fileUrl6, setFileUrl6] = useState("");
+
+
   const [disable1, setDisabled1] = useState(true);
   const [disable2, setDisabled2] = useState(true);
-
+  const [disable3, setDisabled3] = useState(true);
+  const [disable4, setDisabled4] = useState(true);
+  const [disable5, setDisabled5] = useState(true);
+  const [disable6, setDisabled6] = useState(true);
+  
   const history = useHistory();
 
   const onFileChange1 = async (e) => {
@@ -19,15 +42,6 @@ function PersonalHome() {
     await fileRef.put(file);
     setFileUrl1(await fileRef.getDownloadURL());
     setDisabled1(false);
-  };
-
-  const onFileChange2 = async (e) => {
-    const file = e.target.files[0];
-    const storageRef = firebaseApp.storage().ref();
-    const fileRef = storageRef.child(file.name);
-    await fileRef.put(file);
-    setFileUrl2(await fileRef.getDownloadURL());
-    setDisabled2(false);
   };
 
   const deleteFile1 = () => {
@@ -45,6 +59,15 @@ function PersonalHome() {
     setFileUrl1("");
   };
 
+  const onFileChange2 = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebaseApp.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl2(await fileRef.getDownloadURL());
+    setDisabled2(false);
+  };
+
   const deleteFile2 = () => {
     const desertRef = firebaseApp.storage().refFromURL(fileUrl2);
     desertRef
@@ -59,6 +82,106 @@ function PersonalHome() {
     setDisabled1(true);
     setFileUrl1("");
   };
+
+  
+
+  const onFileChange3 = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebaseApp.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl3(await fileRef.getDownloadURL());
+    setDisabled3(false);
+  };
+
+  const deleteFile3 = () => {
+    const desertRef = firebaseApp.storage().refFromURL(fileUrl3);
+    desertRef
+      .delete()
+      .then(function () {
+        alert("Deleted");
+      })
+      .catch(function (error) {
+        alert("Cannot delete");
+      });
+    document.getElementById("file3").value = "";
+    setDisabled3(true);
+    setFileUrl3("");
+  };
+
+  const onFileChange4 = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebaseApp.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl4(await fileRef.getDownloadURL());
+    setDisabled4(false);
+  };
+
+  const deleteFile4 = () => {
+    const desertRef = firebaseApp.storage().refFromURL(fileUrl4);
+    desertRef
+      .delete()
+      .then(function () {
+        alert("Deleted");
+      })
+      .catch(function (error) {
+        alert("Cannot delete");
+      });
+    document.getElementById("file4").value = "";
+    setDisabled4(true);
+    setFileUrl4("");
+  };
+
+
+  const onFileChange5 = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebaseApp.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl5(await fileRef.getDownloadURL());
+    setDisabled5(false);
+  };
+
+  const deleteFile5 = () => {
+    const desertRef = firebaseApp.storage().refFromURL(fileUrl5);
+    desertRef
+      .delete()
+      .then(function () {
+        alert("Deleted");
+      })
+      .catch(function (error) {
+        alert("Cannot delete");
+      });
+    document.getElementById("file5").value = "";
+    setDisabled5(true);
+    setFileUrl5("");
+  };
+
+  const onFileChange6 = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebaseApp.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl6(await fileRef.getDownloadURL());
+    setDisabled6(false);
+  };
+
+  const deleteFile6 = () => {
+    const desertRef = firebaseApp.storage().refFromURL(fileUrl6);
+    desertRef
+      .delete()
+      .then(function () {
+        alert("Deleted");
+      })
+      .catch(function (error) {
+        alert("Cannot delete");
+      });
+    document.getElementById("file6").value = "";
+    setDisabled6(true);
+    setFileUrl6("");
+  };
+
 
   const submitForm = (e) => {
     e.preventDefault();
@@ -81,40 +204,157 @@ function PersonalHome() {
 
   return (
     <div className="personalhome">
-      This is the personal Home screen
-      <form onSubmit={submitForm}>
-        <div>
-          <input
-            type="file"
-            id="file1"
-            placeholder="Your file here"
-            onChange={onFileChange1}
-          />
+      <div className="perosonalhome__box">
+  
+        <div className="personalhome__heading"> Submit the Documents Here </div>
+        <form onSubmit={submitForm} className="personalhome__form">
+          <div className="form__field">
+            <input
+              type="file"
+              id="file1"
+              placeholder="Your file here"
+              onChange={onFileChange1}
+            /> 
 
-          <Button disabled={!fileUrl1} onClick={deleteFile1}>
-            Remove This File
+            <Button
+              disabled={!fileUrl1}
+              onClick={deleteFile1}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+      </Button>
+          </div>
+
+          <div className="form__field">
+            <input
+              type="file"
+              id="file2"
+              placeholder="Your file here"
+              onChange={onFileChange2}
+            />
+
+
+
+            <Button
+              disabled={!fileUrl2}
+              onClick={deleteFile2}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+      </Button>
+          </div>
+
+          <div className="form__field">
+            <input
+              type="file"
+              id="file3"
+              placeholder="Your file here"
+              onChange={onFileChange3}
+            />
+
+
+
+            <Button
+              disabled={!fileUrl3}
+              onClick={deleteFile3}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+      </Button>
+
+
+          </div>
+
+          <div className="form__field">
+            <input
+              type="file"
+              id="file4"
+              placeholder="Your file here"
+              onChange={onFileChange4}
+            />
+
+
+            <Button
+              disabled={!fileUrl4}
+              onClick={deleteFile4}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+      </Button>
+          </div>
+
+          <div className="form__field">
+            <input
+              type="file"
+              id="file5"
+              placeholder="Your file here"
+              onChange={onFileChange5}
+            />
+
+
+
+            <Button
+              disabled={!fileUrl5}
+              onClick={deleteFile5}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+      </Button>
+
+
+          </div>
+
+          <div className="form__field">
+            <input
+              type="file"
+              id="file6"
+              placeholder="Your file here"
+              onChange={onFileChange6}
+            />
+
+            <Button
+              disabled={!fileUrl6}
+              onClick={deleteFile6}
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+             </Button>
+
+
+          </div >
+          
+          <div>
+          <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        disabled={disable1 || disable2 || disable3 || disable4 || disable5 || disable6} type="submit"
+           startIcon={<CloudUploadIcon />}
+      >
+           Upload
           </Button>
-        </div>
-
-        <div>
-          <input
-            type="file"
-            id="file2"
-            placeholder="Your file here"
-            onChange={onFileChange2}
-          />
-
-          <Button disabled={!fileUrl2} onClick={deleteFile2}>
-            Remove This File
-          </Button>
-        </div>
-
-        <div>
-          <button disabled={disable1 || disable2} type="submit">
-            Submit Form
-          </button>
-        </div>
-      </form>
+          </div>
+    
+        </form>
+      </div>
     </div>
   );
 }
