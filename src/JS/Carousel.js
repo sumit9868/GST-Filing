8f7855421cc22fdd0e30f873bc3bdba429e3c18e
garima <vatsgarima11@gmail.com>
2020-11-15 19:32:58 +0530

    carousel final

--- src/JS/Carousel.js
+++ src/JS/Carousel.js
@@ -2,6 +2,10 @@ import { Divider } from "@material-ui/core";
 import React from "react";
 import '../CSS/Carousel.css';
 import banner1 from '../Media/banner1.jpg';
+import banner2 from '../Media/banner2.png';
+import banner3 from '../Media/banner3.jpg';
+import bannerE from '../Media/bannerE.jpg';
+import bannerMSME from '../Media/bannerMSME.jpg';
 
 function Carousel(){
 
@@ -9,15 +13,15 @@ function Carousel(){
         <div class="marquee">
             <ul class="marquee-content">
                 <li><img src={banner1} /></li>
-                <li><img src='https://images.pexels.com/photos/1179863/pexels-photo-1179863.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
-                <li><img src='https://images.pexels.com/photos/934011/pexels-photo-934011.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
-                <li><img src='https://images.pexels.com/photos/545042/pexels-photo-545042.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
-                <li><img src='https://images.pexels.com/photos/60909/rose-yellow-flower-petals-60909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
+                <li><img src={banner2} /></li>
+                <li><img src={banner3} /></li>
+                <li><img src={bannerE} /></li>
+                <li><img src={bannerMSME} /></li>
 
                 <li><img src={banner1} /></li>
-                <li><img src='https://images.pexels.com/photos/1179863/pexels-photo-1179863.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
-                <li><img src='https://images.pexels.com/photos/934011/pexels-photo-934011.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
-                <li><img src='https://images.pexels.com/photos/545042/pexels-photo-545042.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' /></li>
+                <li><img src={banner2} /></li>
+                <li><img src={banner3} /></li>
+                <li><img src={bannerE} /></li>
             </ul>
         </div>
     )
